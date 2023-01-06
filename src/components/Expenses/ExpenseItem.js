b53fc479e606f92599b659a755335667d8f1555b
Ayaka Hibino <ayaka.hibino@gmail.com>
2023-01-06 14:13:05 -0500

    Creates Card component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-
+import { useState } from "react";
+import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
@@ -7,19 +7,19 @@ const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
-    setTitle('UPDATED!');
+    setTitle("UPDATED!");
     console.log("clicked");
   };
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
         <button onClick={clickHandler}>Change title</button>
       </div>
-    </div>
+    </Card>
   );
 };
 
