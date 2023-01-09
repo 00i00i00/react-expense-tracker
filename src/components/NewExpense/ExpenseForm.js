8e8ada09cfb42d26de946b0e2483745c00276f62
Ayaka Hibino <ayaka.hibino@gmail.com>
2023-01-09 10:44:21 -0500

    Hide form functionality

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -33,6 +33,10 @@ const ExpenseForm = (props) => {
     setEnteredDate("");
   };
 
+  const cancelClickHandler = () => {
+    props.onCancelClick();
+  }
+
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
@@ -66,6 +70,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={cancelClickHandler}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,39 @@
+import { useState } from "react";
+
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [formHidden, setFormHidden] = useState(true);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setFormHidden(true);
   };
 
+  const formVisibilityHandler = () => {
+    setFormHidden((prevState) => {
+      return !prevState;
+    });
+  };
+
+  if (formHidden) {
+    return (
+      <div className="new-expense">
+        <button onClick={formVisibilityHandler}>Add New Expense</button>
+      </div>
+    );
+  }
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      <ExpenseForm
+        onSaveExpenseData={saveExpenseDataHandler}
+        onCancelClick={formVisibilityHandler}
+      />
     </div>
   );
 };
